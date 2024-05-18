aeb1519f4110e3394a40b2ed7c5081cbfa01dd54
Betty Chiang <betty2002o@gmail.com>
2024-05-17 22:28:36 -0700

    add movie link and change description with ...

--- src/components/MovieList/MovieCard.jsx
+++ src/components/MovieList/MovieCard.jsx
@@ -6,7 +6,11 @@ const MovieCard = ({ movie }) => {
   console.log(movie);
   const roundedVoteAverage = movie.vote_average.toFixed(1);
   return (
-    <a className="movie_card">
+    <a
+      href={`https://www.themoviedb.org/movie/${movie.id}`}
+      className="movie_card"
+      target="_blank"
+    >
       <img
         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         alt="movie poster"
@@ -21,7 +25,9 @@ const MovieCard = ({ movie }) => {
             <img src={Star} alt="rating icon" className="card_emoji" />
           </p>
         </div>
-        <p className="movie_description">{movie.overview}</p>
+        <p className="movie_description">
+          {movie.overview.slice(0, 100) + "..."}
+        </p>
       </div>
     </a>
   );
