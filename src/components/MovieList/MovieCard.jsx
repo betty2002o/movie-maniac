5c6d1e4f91db86c69850d00bebcc3a7a1d3bf162
Betty Chiang <betty2002o@gmail.com>
2024-05-17 22:23:09 -0700

    added tmdb api, and map out the movies to movie card

--- src/components/MovieList/MovieCard.jsx
+++ src/components/MovieList/MovieCard.jsx
@@ -1,15 +1,30 @@
 import React from "react";
+import Star from "../../assets/glowing-star.png";
+import "./MovieCard.css";
 
-const MovieCard = () => {
-  return <div>MovieCard</div>;
+const MovieCard = ({ movie }) => {
+  console.log(movie);
+  const roundedVoteAverage = movie.vote_average.toFixed(1);
+  return (
+    <a className="movie_card">
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt="movie poster"
+        className="movie_poster"
+      />
+      <div className="movie_details">
+        <h3 className="movie_details_heading">{movie.title}</h3>
+        <div className="movie_date_rate align_center">
+          <p>{movie.release_date}</p>
+          <p className="align_center">
+            {roundedVoteAverage}
+            <img src={Star} alt="rating icon" className="card_emoji" />
+          </p>
+        </div>
+        <p className="movie_description">{movie.overview}</p>
+      </div>
+    </a>
+  );
 };
 
 export default MovieCard;
-
-//movie_details   movie_details_heading  movie_date_rate rate_average movie_description
-
-// <img
-//     src={Star}
-//     alt='rating icon'
-//     className='card_emoji'
-// />
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import "./MovieList.css";
 import "../NavBar/NavBar.css";
 import Fire from "../../assets/fire.png";
 
 const MovieList = () => {
+  const [movieLists, setMovieLists] = useState([]);
+  // const api_key = `${process.env.REACT_APP_TMDB_API_KEY}`;
+  // console.log(api_key);
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZGEyYjBhMDM1NDU3MTFhMjZjYzA1YWQ5Mzc4YTVmYiIsInN1YiI6IjY2NDgyMzUwNjQ3YTFlMDMxNmYyMzYyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.A71E9XKwmm5CQdUNble24a5F3p9D3eg8rxlyXcJz-hk",
+    },
+  };
+
+  const fetchMovies = async () => {
+    try {
+      const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+      if (!apiKey) {
+        throw new Error("API key is missing");
+      }
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok" + response.statusText);
+      }
+      const data = await response.json();
+      setMovieLists(data.results);
+    } catch (error) {
+      console.error("There was a problem with the fetch operation:", error);
+    }
+  };
+
   return (
     <section className="movie_list">
       <header className="movie_filters_header align_center">
@@ -14,9 +49,9 @@ const MovieList = () => {
         </h2>
         <div className="movie_filters_fs align_center">
           <ul className="movie_filter align_center">
-            <li className="movie_filter_item">8+ Stars</li>
-            <li className="movie_filter_item">7+ Stars</li>
-            <li className="movie_filter_item">6+ Stars</li>
+            <li className="movie_filter_item">8+Stars</li>
+            <li className="movie_filter_item">7+Stars</li>
+            <li className="movie_filter_item">6+Stars</li>
           </ul>
 
           <select name="" className="movie_sorting">
@@ -31,12 +66,9 @@ const MovieList = () => {
         </div>
       </header>
       <div className="movie_cards">
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+        {movieLists.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
       </div>
     </section>
   );
